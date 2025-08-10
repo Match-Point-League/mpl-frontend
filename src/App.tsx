bfba7d0096141be9db3aa34858322348d3ce2d03
vrindasehgal <vrinda.s2811@gmail.com>
2025-08-10 13:35:21 +0530

    minor style changes

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
                       <div className="flex space-x-4">
                         <Link
                           to="/signin"
-                          className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700"
+                          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
                         >
                           Sign In
                         </Link>
