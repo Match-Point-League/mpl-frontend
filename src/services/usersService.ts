2768bcd052ae7cf1cdbdeb9cc2b6b3ff88097ee4
riatalwar <ria.talwar2006@gmail.com>
2025-08-14 20:18:16 -0700

    UsersService to update user

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,5 +1,17 @@
 import apiClient from '../config/api';
+import { UpdateUserInput, UserProfile } from '@/types';
 
 export class UsersService {
-  
+  /**
+   * Update user profile information
+   * @param updateData - The user data to update
+   * @returns Promise with the updated user data
+   */
+  public static async updateUser(updateData: UpdateUserInput): Promise<UserProfile> {
+    const response = await apiClient.put<{ user: UserProfile }>('/users/update-user', updateData);
+    if (response.success && response.data) {
+      return response.data!.user as UserProfile;
+    }
+    throw new Error(response.error);
+  }
 }
